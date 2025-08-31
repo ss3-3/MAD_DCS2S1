5f520778a0a873dd4aaf0470fae3a98267c7c799
ss3-3 <sohss-wm24@student.tarc.edu.my>
2025-09-01 05:42:54 +0800

    sample main control - state variable

---
+++ app/src/main/java/com/example/taiwanesehouse/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.taiwanesehouse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.taiwanesehouse.ui.theme.TaiwaneseHouseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaiwaneseHouseTheme {
+                NavigationApp()
+            }
+        }
+    }
+}
+
+enum class Screen {
+    Signup,
+    Menu,
+    Order,
+    Cart,
+    Payment
+}
+
+@Composable
+fun NavigationApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Signup.name) {
+        composable(Screen.Signup.name) {
+            SignUpScreen(
+                navController = navController,
+                name = "Taiwanese House",
+                message = "Please enter your details to log in your account."
+            )
+        }
+        composable(Screen.Menu.name) { Text("Menu Screen") }
+        composable(Screen.Order.name) { Text("Order Screen") }
+        composable(Screen.Cart.name) { Text("Cart Screen") }
+        composable(Screen.Payment.name) { Text("Payment Screen") }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaiwaneseHouseTheme {
+        NavigationApp()
+    }
+}
---
+++ app/src/main/java/com/example/taiwanesehouse/SignUpScreen.kt
@@ -0,0 +1,77 @@
+package com.example.taiwanesehouse
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+
+@Composable
+fun SignUpScreen(
+    navController: NavController,
+    name: String,
+    message: String,
+    modifier: Modifier = Modifier
+) {
+    val logoImage = painterResource(R.drawable.taiwanesehouselogo)
+    val coverImage = painterResource(R.drawable.coverpage)
+
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = logoImage,
+                contentDescription = null,
+                modifier = Modifier.height(90.dp).padding(8.dp)
+            )
+            Column(
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(
+                    text = name,
+                    fontSize = 33.sp,
+                    lineHeight = 16.sp
+                )
+                Text(
+                    text = message,
+                    fontSize = 10.sp
+                )
+            }
+        }
+        Box(modifier = Modifier){
+            Button(onClick = { navController.navigate(Screen.Menu.name) }){
+                //should validate user first
+                //if true, go to menu
+                Text(text = "Menu")
+                //else, display a pop-up message
+            }
+            Image(
+                painter = coverImage,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(800.dp),
+                contentScale = ContentScale.Crop,
+                alpha = 0.5F
+            )
+        }
+    }
+}
